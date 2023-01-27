cafcd351ef2f7d86379eb61c1450bfbc122ddae4
blue-7 <blueseven914@gmail.com>
2023-01-28 02:10:41 +0900

    Updated.

--- MultiToneGenerator/MultiToneGenerator.cpp
+++ MultiToneGenerator/MultiToneGenerator.cpp
@@ -103,7 +103,7 @@ public:
 			{
 				v *= INT32_MAX;
 				v = 0 <= v ? v + 0.5 : v - 0.5;
-				v64 = v;
+				v64 = (int64_t)v;
 
 				int32_t value;
 				if (v64 <= INT32_MAX)
@@ -132,11 +132,11 @@ public:
 
 		if (!clip)
 		{
-			printf("FileSaved: %s, Peak Level = %.1f dB\n", path.c_str(), log10f(peak_level) * 20.0);
+			printf("FileSaved: %s, Peak Level = %.1f dB\n", path.c_str(), log10(peak_level) * 20.0);
 		}
 		else
 		{
-			printf("FileSaved: %s, Peak Level = %.1f dB, CLIPPED!!\n", path.c_str(), log10f(peak_level) * 20.0);
+			printf("FileSaved: %s, Peak Level = %.1f dB, CLIPPED!!\n", path.c_str(), log10(peak_level) * 20.0);
 		}
 		return  true;
 	}
@@ -175,11 +175,10 @@ WAVE    generate_wave(uint32_t sampling_freq, uint32_t duration, std::vector<tag
 }
 
 std::vector<tagTONE>	get_tone_1kHz();
-std::vector<tagTONE>	get_tone_standard_imd();
+std::vector<tagTONE>	get_tone_smpte_60_7000();
 std::vector<tagTONE>	get_tone_piano88();
-std::vector<tagTONE>	get_tone_piano88_ACDF();
-std::vector<tagTONE>	get_tone_piano88_ACF();
-std::vector<tagTONE>	get_tone_20();
+std::vector<tagTONE>	get_tone_32();
+std::vector<tagTONE>	get_tone_20_uneven();
 
 int main( int argc, char* argv[])
 {
@@ -197,13 +196,11 @@ int main( int argc, char* argv[])
 		freq = std::atoi(argv[1]);
 	}
 
-	generate_wave(freq, len, get_tone_1kHz()).save_file("Sine_1kHz.wav");
-	generate_wave(freq, len, get_tone_standard_imd()).save_file("IMD_60Hz_7kHz.wav");
-	generate_wave(freq, len, get_tone_20()).save_file("MultiTone_20.wav");
-
-	generate_wave(freq, len, get_tone_piano88()).save_file("MultiTone_piano88.wav");
-	generate_wave(freq, len, get_tone_piano88_ACDF()).save_file("MultiTone_piano88_ACD#F#.wav");
-	generate_wave(freq, len, get_tone_piano88_ACF()).save_file("MultiTone_piano88_AC#F.wav");
+	generate_wave(freq, len, get_tone_1kHz()).save_file("1_Sine_1kHz.wav");
+	generate_wave(freq, len, std::vector<tagTONE>() ).save_file("2_Silent.wav");
+	generate_wave(freq, len, get_tone_smpte_60_7000()).save_file("3_SMPTE_60Hz_7kHz.wav");
+	generate_wave(freq, len, get_tone_32()).save_file("4_MultiTone_32.wav");
+	generate_wave(freq, len, get_tone_20_uneven()).save_file("5_MultiTone_20uneven.wav");
 
 	return  0;
 }
@@ -228,7 +225,7 @@ std::vector<tagTONE>   get_tone_1kHz()
 	return  tone;
 }
 
-std::vector<tagTONE>   get_tone_standard_imd()
+std::vector<tagTONE>   get_tone_smpte_60_7000()
 {
 	std::initializer_list<tagTONE>  tone =
 	{
@@ -236,11 +233,11 @@ std::vector<tagTONE>   get_tone_standard_imd()
 		{ -30.0, 7000 }
 	};
 
-return  tone;
+	return  tone;
 }
 
 
-std::vector<tagTONE>   get_tone_20()
+std::vector<tagTONE>   get_tone_20_uneven()
 {
 	std::initializer_list<tagTONE>  tone =
 	{
@@ -266,175 +263,36 @@ std::vector<tagTONE>   get_tone_20()
 		{ -20.0, 15000 },
 	};
 
-
 	return  tone;
 }
 
+
+std::vector<tagTONE>	get_tone_32()
+{
+	std::vector<tagTONE>	tone;
+
+	for (int32_t i = -18; i <= 13; i++)
+	{
+		tagTONE	t;
+		t.gain = i == 0 ? -20 : -26;
+		t.freq = pow(2, i /3.0)*1000.0;
+		tone.push_back(t);
+	}
+
+	return	tone;
+}
 
 std::vector<tagTONE>   get_tone_piano88()
 {
-	std::initializer_list<tagTONE>  tone =
+	std::vector<tagTONE>	tone;
+
+	for (uint32_t i = 0; i < 88; i++)
 	{
-		{ -36.0,   27.500 },	// 	A0
-		{ -36.0,   29.135 },	// 	A#0
-		{ -36.0,   30.868 },	// 	B0
-		{ -36.0,   32.703 },	// 	C1
-		{ -36.0,   34.648 },	// 	C#1
-		{ -36.0,   36.708 },	// 	D1
-		{ -36.0,   38.891 },	// 	D#1
-		{ -36.0,   41.203 },	// 	E1
-		{ -36.0,   43.654 },	// 	F1
-		{ -36.0,   46.249 },	// 	F#1
-		{ -36.0,   48.999 },	// 	G1
-		{ -36.0,   51.913 },	// 	G#1
-		{ -36.0,   55.000 },	// 	A1
-		{ -36.0,   58.270 },	// 	A#1
-		{ -36.0,   61.735 },	// 	B1
-		{ -36.0,   65.406 },	// 	C2
-		{ -36.0,   69.296 },	// 	C#2
-		{ -36.0,   73.416 },	// 	D2
-		{ -36.0,   77.782 },	// 	D#2
-		{ -36.0,   82.407 },	// 	E2
-		{ -36.0,   87.307 },	// 	F2
-		{ -36.0,   92.499 },	// 	F#2
-		{ -36.0,   97.999 },	// 	G2
-		{ -36.0,  103.826 },	// 	G#2
-		{ -36.0,  110.000 },	// 	A2
-		{ -36.0,  116.541 },	// 	A#2
-		{ -36.0,  123.471 },	// 	B2
-		{ -36.0,  130.813 },	// 	C3
-		{ -36.0,  138.591 },	// 	C#3
-		{ -36.0,  146.832 },	// 	D3
-		{ -36.0,  155.563 },	// 	D#3
-		{ -36.0,  164.814 },	// 	E3
-		{ -36.0,  174.614 },	// 	F3
-		{ -36.0,  184.997 },	// 	F#3
-		{ -36.0,  195.998 },	// 	G3
-		{ -36.0,  207.652 },	// 	G#3
-		{ -36.0,  220.000 },	// 	A3
-		{ -36.0,  233.082 },	// 	A#3
-		{ -36.0,  246.942 },	// 	B3
-		{ -36.0,  261.626 },	// 	C4
-		{ -36.0,  277.183 },	// 	C#4
-		{ -36.0,  293.665 },	// 	D4
-		{ -36.0,  311.127 },	// 	D#4
-		{ -36.0,  329.628 },	// 	E4
-		{ -36.0,  349.228 },	// 	F4
-		{ -36.0,  369.994 },	// 	F#4
-		{ -36.0,  391.995 },	// 	G4
-		{ -36.0,  415.305 },	// 	G#4
-		{ -30.0,  440.000 },	// 	A4
-		{ -36.0,  466.164 },	// 	A#4
-		{ -36.0,  493.883 },	// 	B4
-		{ -36.0,  523.251 },	// 	C5
-		{ -36.0,  554.365 },	// 	C#5
-		{ -36.0,  587.330 },	// 	D5
-		{ -36.0,  622.254 },	// 	D#5
-		{ -36.0,  659.255 },	// 	E5
-		{ -36.0,  698.456 },	// 	F5
-		{ -36.0,  739.989 },	// 	F#5
-		{ -36.0,  783.991 },	// 	G5
-		{ -36.0,  830.609 },	// 	G#5
-		{ -36.0,  880.000 },	// 	A5
-		{ -36.0,  932.328 },	// 	A#5
-		{ -36.0,  987.767 },	// 	B5
-		{ -36.0, 1046.502 },	// 	C6
-		{ -36.0, 1108.731 },	// 	C#6
-		{ -36.0, 1174.659 },	// 	D6
-		{ -36.0, 1244.508 },	// 	D#6
-		{ -36.0, 1318.510 },	// 	E6
-		{ -36.0, 1396.913 },	// 	F6
-		{ -36.0, 1479.978 },	// 	F#6
-		{ -36.0, 1567.982 },	// 	G6
-		{ -36.0, 1661.219 },	// 	G#6
-		{ -36.0, 1760.000 },	// 	A6
-		{ -36.0, 1864.655 },	// 	A#6
-		{ -36.0, 1975.533 },	// 	B6
-		{ -36.0, 2093.005 },	// 	C7
-		{ -36.0, 2217.461 },	// 	C#7
-		{ -36.0, 2349.318 },	// 	D7
-		{ -36.0, 2489.016 },	// 	D#7
-		{ -36.0, 2637.020 },	// 	E7
-		{ -36.0, 2793.826 },	// 	F7
-		{ -36.0, 2959.955 },	// 	F#7
-		{ -36.0, 3135.963 },	// 	G7
-		{ -36.0, 3322.438 },	// 	G#7
-		{ -36.0, 3520.000 },	// 	A7
-		{ -36.0, 3729.310 },	// 	A#7
-		{ -36.0, 3951.066 },	// 	B7
-		{ -36.0, 4186.009 },	// 	C8
-	};
-
-	return  tone;
-}
-
-
-std::vector<tagTONE>   get_tone_piano88_ACDF()
-{
-	std::initializer_list<tagTONE>  tone =
-	{
-		{ -36.0,   27.500 },	// 	A0
-		{ -36.0,   32.703 },	// 	C1
-		{ -36.0,   38.891 },	// 	D#1
-		{ -36.0,   46.249 },	// 	F#1
-		{ -36.0,   55.000 },	// 	A1
-		{ -36.0,   65.406 },	// 	C2
-		{ -36.0,   77.782 },	// 	D#2
-		{ -36.0,   92.499 },	// 	F#2
-		{ -36.0,  110.000 },	// 	A2
-		{ -36.0,  130.813 },	// 	C3
-		{ -36.0,  155.563 },	// 	D#3
-		{ -36.0,  184.997 },	// 	F#3
-		{ -36.0,  220.000 },	// 	A3
-		{ -36.0,  261.626 },	// 	C4
-		{ -36.0,  311.127 },	// 	D#4
-		{ -36.0,  369.994 },	// 	F#4
-		{ -30.0,  440.000 },	// 	A4
-		{ -36.0,  523.251 },	// 	C5
-		{ -36.0,  622.254 },	// 	D#5
-		{ -36.0,  739.989 },	// 	F#5
-		{ -36.0,  880.000 },	// 	A5
-		{ -36.0, 1046.502 },	// 	C6
-		{ -36.0, 1244.508 },	// 	D#6
-		{ -36.0, 1479.978 },	// 	F#6
-		{ -36.0, 1760.000 },	// 	A6
-		{ -36.0, 2093.005 },	// 	C7
-		{ -36.0, 2489.016 },	// 	D#7
-		{ -36.0, 2959.955 },	// 	F#7
-		{ -36.0, 3520.000 },	// 	A7
-		{ -36.0, 4186.009 },	// 	C8
-	};
-
-	return  tone;
-}
-
-std::vector<tagTONE>   get_tone_piano88_ACF()
-{
-	std::initializer_list<tagTONE>  tone =
-	{
-		{ -36.0,   27.500 },	// 	A0
-		{ -36.0,   34.648 },	// 	C#1
-		{ -36.0,   43.654 },	// 	F1
-		{ -36.0,   55.000 },	// 	A1
-		{ -36.0,   69.296 },	// 	C#2
-		{ -36.0,   87.307 },	// 	F2
-		{ -36.0,  110.000 },	// 	A2
-		{ -36.0,  138.591 },	// 	C#3
-		{ -36.0,  174.614 },	// 	F3
-		{ -36.0,  220.000 },	// 	A3
-		{ -36.0,  277.183 },	// 	C#4
-		{ -36.0,  349.228 },	// 	F4
-		{ -30.0,  440.000 },	// 	A4
-		{ -36.0,  554.365 },	// 	C#5
-		{ -36.0,  698.456 },	// 	F5
-		{ -36.0,  880.000 },	// 	A5
-		{ -36.0, 1108.731 },	// 	C#6
-		{ -36.0, 1396.913 },	// 	F6
-		{ -36.0, 1760.000 },	// 	A6
-		{ -36.0, 2217.461 },	// 	C#7
-		{ -36.0, 2793.826 },	// 	F7
-		{ -36.0, 3520.000 },	// 	A7
-	};
+		tagTONE	t;
+		t.gain = -36;
+		t.freq = pow(2, i / 12.0 ) * 27.5;
+		tone.push_back(t);
+	}
 
 	return  tone;
 }
